66565dc3312026412c552f01f056ba39dec7880d
enorsu <pelaaja538@gmail.com>
2025-07-04 22:14:42 +0300

    yes

--- public/js/copy.js
+++ public/js/copy.js
@@ -1,5 +1,4 @@
 const btn = document.getElementById("copy");
 btn.addEventListener("click", function() {
     navigator.clipboard.writeText("https://vi.dy.fi/" + btn.dataset.a)
-    alert("copied the url")
 })
